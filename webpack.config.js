7e842afb9ca7a56a89d3cfac420cf1753b786216
Nastassia Ananka <anast.ananko@gmail.com>
2023-07-06 15:56:22 +0200

    feat: add basic configs

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+module.exports = {
+  mode: mode,
+  entry: './index.ts',
+  target: 'node',
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+};
