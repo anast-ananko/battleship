8d0d92bb1e2b9b4b672cceeae1d621bdae90326b
Nastassia Ananka <anast.ananko@gmail.com>
2023-07-08 15:47:09 +0200

    feat: add fields

--- src/game/index.ts
+++ src/game/index.ts
@@ -13,12 +13,18 @@ interface IShipsPlayer {
   ships: IShipData[];
 }
 
+interface IField {
+  playerIndex: number;
+  field: string[][];
+}
+
 export class BattleshipGame {
   static id = 0;
   gameId: number;
   currentPlayer: number | null = null;
   shipsForPlayer: IShipsPlayer[] = [];
-  ships: IShipData[] = [];
+  fieldsForPlayer: IField[] = [];
+  //ships: IShipData[] = [];
 
   constructor() {
     this.gameId = BattleshipGame.id;
@@ -34,5 +40,43 @@ export class BattleshipGame {
       playerIndex: idPlayer,
       ships,
     });
+
+    this.createField(idPlayer, ships);
+  }
+
+  createField(idPlayer: number, ships: IShipData[]): void {
+    const gameBoard: string[][] = [];
+
+    for (let y = 0; y < 10; y++) {
+      gameBoard[y] = [];
+
+      for (let x = 0; x < 10; x++) {
+        gameBoard[y][x] = 'none';
+      }
+    }
+
+    ships.forEach((ship) => {
+      const { position, direction, length } = ship;
+      const { x, y } = position;
+
+      if (direction) {
+        if (x + length <= 10) {
+          for (let i = 0; i < length; i++) {
+            gameBoard[y + i][x] = 'healthy';
+          }
+        }
+      } else {
+        if (y + length <= 10) {
+          for (let i = 0; i < length; i++) {
+            gameBoard[y][x + i] = 'healthy';
+          }
+        }
+      }
+    });
+
+    this.fieldsForPlayer.push({ playerIndex: idPlayer, field: gameBoard });
+
+    // console.log(gameBoard);
+    // console.log('\n\n');
   }
 }
